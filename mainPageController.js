5177b99b817b9210384640c5163f689af0a0b123
Matej <1337pinter@gmail.com>
2017-09-22 13:42:59 +0200

    Added limitations and output of C class

--- mainPageController.js
+++ mainPageController.js
@@ -11,6 +11,8 @@ mainPageController = (function () {
             app = new Vue({
                 el: '#app',
                 data: {
+                    selectedPage: 0,
+
                     roadCategory: 0,
                     speedLimit: 0,
                     trafficDensity: 0,
@@ -19,9 +21,25 @@ mainPageController = (function () {
                     crossingDensity: 0,
                     hasParkedVehicles: false,
                     ambientLight: 0,
-                    roadDifficulty: 0
+                    roadDifficulty: 0,
+
+                    pTravelSpeed:0,
+                    pUsageInt: 0,
+                    pTrafficConst: 0,
+                    pHasParkedVehicles: false,
+                    pAmbilight: 0,
+                    pFaceRecognition: 0
                 },
                 computed: {
+                    selectedPageTitle: function () {
+                        switch (this.selectedPage) {
+                            case 0:
+                                return "Določitev M in C razreda razsvetljave";
+                            case 1:
+                                return "Določitev P razreda razsvetljave";
+                        }
+                        return "";
+                    },
                     trafficDensityHighOptionLabel: function () {
                         switch (parseInt(this.roadCategory)) {
                             case 0:
@@ -62,7 +80,7 @@ mainPageController = (function () {
                         }
                         return 0;
                     },
-                    mTrafficDensityScore: function () {
+                    mcTrafficDensityScore: function () {
                         switch (parseInt(this.trafficDensity)) {
                             case 0:
                                 return 1;
@@ -73,7 +91,7 @@ mainPageController = (function () {
                         }
                         return 0;
                     },
-                    mTrafficConstitutionScore: function () {
+                    mcTrafficConstitutionScore: function () {
                         switch (parseInt(this.trafficConstitution)) {
                             case 0:
                                 return 2;
@@ -84,7 +102,7 @@ mainPageController = (function () {
                         }
                         return 0;
                     },
-                    mLaneSepScore: function () {
+                    mcLaneSepScore: function () {
                         return this.laneSep == true ? 0 : 1;
                     },
                     mCrossingDensityScore: function () {
@@ -96,10 +114,10 @@ mainPageController = (function () {
                         }
                         return 0;
                     },
-                    mHasParkedVehiclesScore: function () {
+                    mcHasParkedVehiclesScore: function () {
                         return this.hasParkedVehicles == true ? 1 : 0;
                     },
-                    mAmbientLightScore: function () {
+                    mcAmbientLightScore: function () {
                         switch (parseInt(this.ambientLight)) {
                             case 0:
                                 return 1;
@@ -110,7 +128,7 @@ mainPageController = (function () {
                         }
                         return 0;
                     },
-                    mRoadDifficultyScore: function () {
+                    mcRoadDifficultyScore: function () {
                         switch (parseInt(this.roadDifficulty)) {
                             case 0:
                                 return 2;
@@ -123,13 +141,13 @@ mainPageController = (function () {
                     },
                     mVWS: function () {
                         return this.mSpeedLimitScore +
-                            this.mTrafficDensityScore +
-                            this.mTrafficConstitutionScore +
-                            this.mLaneSepScore +
+                            this.mcTrafficDensityScore +
+                            this.mcTrafficConstitutionScore +
+                            this.mcLaneSepScore +
                             this.mCrossingDensityScore +
-                            this.mHasParkedVehiclesScore +
-                            this.mAmbientLightScore +
-                            this.mRoadDifficultyScore;
+                            this.mcHasParkedVehiclesScore +
+                            this.mcAmbientLightScore +
+                            this.mcRoadDifficultyScore;
                     },
                     mClassNo: function () {
                         var classVWS = this.mVWS < 0 ? 0 : this.mVWS;
@@ -200,78 +218,28 @@ mainPageController = (function () {
                         }
                         return 0;
                     },
-                    cTrafficDensityScore: function () {
-                        switch (parseInt(this.trafficDensity)) {
-                            case 0:
-                                return 1;
-                            case 1:
-                                return 0;
-                            case 2:
-                                return -1;
-                        }
-                        return 0;
-                    },
-                    cTrafficConstitutionScore: function () {
-                        switch (parseInt(this.trafficConstitution)) {
-                            case 0:
-                                return 2;
-                            case 1:
-                                return 1;
-                            case 2:
-                                return 0;
-                        }
-                        return 0;
-                    },
-                    cLaneSepScore: function () {
-                        return this.laneSep == true ? 0 : 1;
-                    },
-                    cHasParkedVehiclesScore: function () {
-                        return this.hasParkedVehicles == true ? 1 : 0;
-                    },
-                    cAmbientLightScore: function () {
-                        switch (parseInt(this.ambientLight)) {
-                            case 0:
-                                return 1;
-                            case 1:
-                                return 0;
-                            case 2:
-                                return -1;
-                        }
-                        return 0;
-                    },
-                    cRoadDifficultyScore: function () {
-                        switch (parseInt(this.roadDifficulty)) {
-                            case 0:
-                                return 2;
-                            case 1:
-                                return 1;
-                            case 2:
-                                return 0;
-                        }
-                        return 0;
-                    },
                     cVWS: function () {
                         return this.cSpeedLimitScore +
-                            this.cTrafficDensityScore +
-                            this.cTrafficConstitutionScore +
-                            this.cLaneSepScore +
-                            this.cHasParkedVehiclesScore +
-                            this.cAmbientLightScore +
-                            this.cRoadDifficultyScore;
+                            this.mcTrafficDensityScore +
+                            this.mcTrafficConstitutionScore +
+                            this.mcLaneSepScore +
+                            this.mcHasParkedVehiclesScore +
+                            this.mcAmbientLightScore +
+                            this.mcRoadDifficultyScore;
                     },
                     cClassNo: function () {
                         var classVWS = this.cVWS < 0 ? 0 : this.cVWS;
                         var classNo = 6 - classVWS;
 
-                        if (classNo <= 0)
-                            return 1;
+                        if (classNo < 0)
+                            return 0;
                         return classNo;
                     },
                     cClassStr: function () {
                         if (this.cClassNo >= 0 && this.cClassNo <= 5) {
                             var result = '<table class="table table-striped table-condensed table-hover"><thead><tr>';
                             result += '<th>' + "Razred" + '</th>';
-                            result += '<th><i>E<sub>sr</sub> &nbsp;(I<sub>x</sub>)</i></th>';
+                            result += '<th><i>E<sub>sr</sub> &nbsp;(L<sub>x</sub>)</i></th>';
                             result += '<th><i>U<sub>O</sub></i></th>';
                             result += "</tr></thead>";
                             result += "<tbody>";
@@ -305,6 +273,125 @@ mainPageController = (function () {
                             return result;
                         } else {
                             return '<span class="glyphicon glyphicon-alert text-danger"></span> Vrednost parametrov ni primerna za razred razsvetljave C'
+                        }
+
+                        return result;
+                    },
+                    pTravelSpeedScore: function () {
+                        switch (parseInt(this.pTravelSpeed)) {
+                            case 0:
+                                return 1;
+                            case 1:
+                                return 0;
+                        }
+                        return 0;
+                    },
+                    pUsageIntScore: function () {
+                        switch (parseInt(this.pUsageInt)) {
+                            case 0:
+                                return 1;
+                            case 1:
+                                return 0;
+                            case 2:
+                                return -1;
+                        }
+                        return 0;
+                    },
+                    pTrafficConstScore: function () {
+                        switch (parseInt(this.pTrafficConst)) {
+                            case 0:
+                                return 2;
+                            case 1:
+                                return 1;
+                            case 2:
+                                return 1;
+                            case 3:
+                                return 0;
+                            case 4:
+                                return 0;
+                        }
+                        return 0;
+                    },
+                    pHasParkedVehiclesScore: function () {
+                        return this.pHasParkedVehicles == true ? 1 : 0;
+                    },
+                    pAmbilightScore: function () {
+                        switch (parseInt(this.pAmbilight)) {
+                            case 0:
+                                return 1;
+                            case 1:
+                                return 0;
+                            case 2:
+                                return -1;
+                        }
+                        return 0;
+                    },
+                    pFaceRecognitionScore: function () {
+                        switch (parseInt(this.pFaceRecognition)) {
+                            case 0:
+                                return "Dodatne zahteve";
+                            case 1:
+                                return "Brez dodatnih zahtev";
+                        }
+                        return "Brez dodatnih zahtev";
+                    },
+                    pVWS: function () {
+                        return this.pTravelSpeedScore +
+                            this.pUsageIntScore +
+                            this.pTrafficConstScore +
+                            this.pHasParkedVehiclesScore +
+                            this.pAmbilightScore;
+                    },
+                    pClassNo: function () {
+                        var classVWS = this.pVWS < 0 ? 0 : this.pVWS;
+                        var classNo = 6 - classVWS;
+
+                        if (classNo <= 0)
+                            return 1;
+                        return classNo;
+                    },
+                    pClassStr: function () {
+                        if (this.pClassNo >= 1 && this.pClassNo <= 7) {
+                            var result = '<table class="table table-striped table-condensed table-hover"><thead><tr>';
+                            result += '<th>' + "Razred" + '</th>';
+                            result += '<th><i>Povpr. vod. osv.&nbsp;(L<sub>x</sub>)</i></th>';
+                            result += '<th><i>min. vod. osv.&nbsp;(L<sub>x</sub>)</i></th>';
+                            result += "</tr></thead>";
+                            result += "<tbody>";
+
+                            if (this.pClassNo == 1) {
+                                result += "<tr><td><b>P1</b></td><td>15</td><td>5</td></tr>";
+                            }
+
+                            if (this.pClassNo == 2) {
+                                result += "<tr><td><b>P2</b></td><td>10</td><td>3</td></tr>";
+                            }
+
+                            if (this.pClassNo == 3) {
+                                result += "<tr><td><b>P3</b></td><td>7,5</td><td>1,5</td></tr>";
+                            }
+
+                            if (this.pClassNo == 4) {
+                                result += "<tr><td><b>P4</b></td><td>5</td><td>1</td></tr>";
+                            }
+
+                            if (this.pClassNo == 5) {
+                                result += "<tr><td><b>P5</b></td><td>3</td><td>0,6</td></tr>";
+                            }
+
+                            if (this.pClassNo == 6) {
+                                result += "<tr><td><b>P6</b></td><td>2</td><td>0,6</td></tr>";
+                            }
+
+                            if (this.pClassNo == 7) {
+                                result += "<tr><td><b>P7</b></td><td>ni zahtev</td><td>ni zahtev</td></tr>";
+                            }
+
+                            result += "</tbody>";
+                            result += "</table>";
+                            return result;
+                        } else {
+                            return '<span class="glyphicon glyphicon-alert text-danger"></span> Vrednost parametrov ni primerna za razred razsvetljave P'
                         }
 
                         return result;
